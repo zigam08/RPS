2c5652295b5b8da96f7854a683b6f0e4428df41e
zigam08 <ziga.mihelic2008@gmail.com>
2026-09-03 09:59:41 +0000

    2 vaja

--- vaje/uvod.py
+++ vaje/uvod.py
@@ -5,6 +5,13 @@ def hello():
     else:
         print(f"Hello {oddelek}")
 
+def poštevanka():
+    x = int(input("Izberi si število: "))
+    št = 1
+    while št <= 10:
+        print(f"{št} * {x} = {št * x}")
+        št += 1
 
 if __name__ == "__main__":
-    hello()
+    # hello()
+    poštevanka()
