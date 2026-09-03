8d96f7961fcd9aa6a5b7503494fef81a8bd34be4
zigam08 <ziga.mihelic2008@gmail.com>
2026-09-03 09:46:49 +0000

    osnova py

--- vaje/uvod.py
+++ vaje/uvod.py
@@ -1,1 +1,10 @@
-print("Hello 1.Ri")
+def hello():
+    oddelek = input("Kateri oddelek si? ")
+    if oddelek.lower() == "1.ri":
+        print(f"Hello {oddelek} ♥")
+    else:
+        print(f"Hello {oddelek}")
+
+
+if __name__ == "__main__":
+    hello()
